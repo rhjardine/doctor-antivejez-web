fe9ce5e0ec25d472652ecc5d7079c05a7180b185
Jardine <bc222110@bancaribe.com.ve>
2026-01-23 16:31:42 -0400

    Explicitly allow mobile routes in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,9 @@
 import { NextResponse } from "next/server";
-export function middleware() { return NextResponse.next(); }
+export function middleware(request: any) {
+    const { pathname } = request.nextUrl;
+    if (pathname.startsWith('/mobile-auth-v1') || pathname.startsWith('/mobile-profile-v1')) {
+        return NextResponse.next();
+    }
+    return NextResponse.next();
+}
 export const config = { matcher: [] };
